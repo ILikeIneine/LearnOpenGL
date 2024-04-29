24c02c9efd98e4905fd491056374f82a1e26c8ad
Hank <hcc.mayday@gmail.com>
2024-04-29 09:59:19 +0800

    fix: :bug: texture coordinates mistakes

--- Texture/load_texture.cpp
+++ Texture/load_texture.cpp
@@ -13,240 +13,246 @@ constexpr unsigned int SCR_HEIGHT = 800;
 
 float mixValue = 0.2f;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
 
 void processInput(GLFWwindow* window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-	{
-		mixValue += 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
-		if (mixValue >= 1.0f)
-			mixValue = 1.0f;
-	}
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-	{
-		mixValue -= 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
-		if (mixValue <= 0.0f)
-			mixValue = 0.0f;
-	}
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        mixValue += 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        if (mixValue >= 1.0f)
+            mixValue = 1.0f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        mixValue -= 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        if (mixValue <= 0.0f)
+            mixValue = 0.0f;
+    }
 }
 
 int main()
 {
-	//-------------------------------------
-	// glfw: initialize and configure
-	//-------------------------------------
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //-------------------------------------
+    // glfw: initialize and configure
+    //-------------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-	//-------------------------------------
-	// glfw window creation
-	//-------------------------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
-	if (window == nullptr)
-	{
-		std::cout << "Failed to create GLFW window\n";
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    //-------------------------------------
+    // glfw window creation
+    //-------------------------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	//----------------------------------------
-	// glad: load all OpenGL function pointers
-	//----------------------------------------
-	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
-	{
-		std::cout << "Failed to initialize GLAD\n";
-		return -1;
-	}
+    //----------------------------------------
+    // glad: load all OpenGL function pointers
+    //----------------------------------------
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+    {
+        std::cout << "Failed to initialize GLAD\n";
+        return -1;
+    }
 
-	//-------------------------------------
-	// Create shader
-	//-------------------------------------
-	const auto triangle_vs = std::filesystem::current_path() / "../../../../Texture/shaders/triangle.vs";
-	const auto triangle_fs = std::filesystem::current_path() / "../../../../Texture/shaders/triangle.fs";
-	const auto axis_vs = std::filesystem::current_path() / "../../../../Texture/shaders/axis.vs";
-	const auto axis_fs = std::filesystem::current_path() / "../../../../Texture/shaders/axis.fs";
+    //-------------------------------------
+    // Create shader
+    //-------------------------------------
+    const auto triangle_vs = std::filesystem::current_path() / "../../../../Texture/shaders/triangle.vs";
+    const auto triangle_fs = std::filesystem::current_path() / "../../../../Texture/shaders/triangle.fs";
+    const auto axis_vs = std::filesystem::current_path() / "../../../../Texture/shaders/axis.vs";
+    const auto axis_fs = std::filesystem::current_path() / "../../../../Texture/shaders/axis.fs";
 
-	Shader rectangleShader(triangle_vs, triangle_fs);
-	Shader axisShader(axis_vs, axis_fs);
+    Shader rectangleShader(triangle_vs, triangle_fs);
+    Shader axisShader(axis_vs, axis_fs);
 
-	constexpr float axis_vertices[] = {
-		-1.0, 0.0, 0.0,
-		1.0, 0.0, 0.0,
-		0.0, -1.0, 0.0,
-		0.0, 1.0, 0.0
-	};
+    constexpr float axis_vertices[] = {
+        -1.0, 0.0, 0.0,
+        1.0, 0.0, 0.0,
+        0.0, -1.0, 0.0,
+        0.0, 1.0, 0.0
+    };
 
-	constexpr unsigned int axis_indices[] = {
-		0, 1,
-		2, 3
-	};
+    constexpr unsigned int axis_indices[] = {
+        0, 1,
+        2, 3
+    };
 
-	float rectangle_vertices[] = {
-		//     ---- pos ----       ---- color ----     - texture -
-	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
-	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
-	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
-	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
-	};
+    float rectangle_vertices[] = {
+        //     ---- pos ----       ---- color ----     - texture -
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // 右上
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // 右下
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // 左下
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f // 左上
+    };
 
-	unsigned int rectangle_indices[] = {
-		0, 1, 3, // first triangle
-		1, 2, 3  // second triangle
-	};
-
-
-	//-------------------------------------------------------------------------
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), 
-	// and then configure vertex attributes(s).
-	//-------------------------------------------------------------------------
-	unsigned int vao[2];
-	unsigned int vbo[2];
-	unsigned int ebo[2];
-	glGenVertexArrays(2, vao);
-	glGenBuffers(2, vbo);
-	glGenBuffers(2, ebo);
-	auto trianglePosAttrib = glGetAttribLocation(rectangleShader.prog_id(), "aPos");
-	auto triangleColorAttrib = glGetAttribLocation(rectangleShader.prog_id(), "aColor");
-	auto triangleTexCoordAttrib = glGetAttribLocation(rectangleShader.prog_id(), "aTexCoord");
-	auto axisPosAttrib = glGetAttribLocation(axisShader.prog_id(), "aPos");
-
-	//-------------------------------------
-	// Bind axis
-	//-------------------------------------
-	glBindVertexArray(vao[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(axis_vertices), axis_vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[0]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(axis_indices), axis_indices, GL_STATIC_DRAW);
-	// Attributes
-	glVertexAttribPointer(axisPosAttrib, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
-	glEnableVertexAttribArray(axisPosAttrib);
-
-	//-------------------------------------
-	// Bind triangle
-	//-------------------------------------
-	glBindVertexArray(vao[1]);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle_vertices), rectangle_vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[1]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rectangle_indices), rectangle_indices, GL_STATIC_DRAW);
-	// Attributes
-	glVertexAttribPointer(trianglePosAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), static_cast<void*>(nullptr));
-	glEnableVertexAttribArray(trianglePosAttrib);
-	glVertexAttribPointer(triangleColorAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
-	glEnableVertexAttribArray(triangleColorAttrib);
-	glVertexAttribPointer(triangleTexCoordAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
-	glEnableVertexAttribArray(triangleTexCoordAttrib);
+    unsigned int rectangle_indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3 // second triangle
+    };
 
 
-	//-------------------------------------
-	// load and create a texture
-	//-------------------------------------
-	unsigned int texture1;
-	glGenTextures(1, &texture1);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-	// Set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	// Set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	{
-		int width, height, nrChannels;
-		//stbi_set_flip_vertically_on_load(true);
-		unsigned char* data = stbi_load(R"(C:\Users\m01016\Git\LearnOpenGL\Texture\textures\container.jpg)", &width, &height, &nrChannels, 0);
+    //-------------------------------------------------------------------------
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), 
+    // and then configure vertex attributes(s).
+    //-------------------------------------------------------------------------
+    unsigned int vao[2];
+    unsigned int vbo[2];
+    unsigned int ebo[2];
+    glGenVertexArrays(2, vao);
+    glGenBuffers(2, vbo);
+    glGenBuffers(2, ebo);
+    auto trianglePosAttrib = glGetAttribLocation(rectangleShader.prog_id(), "aPos");
+    auto triangleColorAttrib = glGetAttribLocation(rectangleShader.prog_id(), "aColor");
+    auto triangleTexCoordAttrib = glGetAttribLocation(rectangleShader.prog_id(), "aTexCoord");
+    auto axisPosAttrib = glGetAttribLocation(axisShader.prog_id(), "aPos");
 
-		if (data)
-		{
-			glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0,GL_RGB, GL_UNSIGNED_BYTE, data);
-			glGenerateMipmap(GL_TEXTURE_2D);
-		}
-		else
-		{
-			std::cout << "Failed to load texture\n";
-		}
+    //-------------------------------------
+    // Bind axis
+    //-------------------------------------
+    glBindVertexArray(vao[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(axis_vertices), axis_vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(axis_indices), axis_indices, GL_STATIC_DRAW);
+    // Attributes
+    glVertexAttribPointer(axisPosAttrib, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
+    glEnableVertexAttribArray(axisPosAttrib);
 
-		stbi_image_free(data);
-	}
-
-	unsigned int texture2;
-	glGenTextures(1, &texture2);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	// Set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// Set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-	{
-		int width, height, nrChannels;
-		//stbi_set_flip_vertically_on_load(true);
-		unsigned char* data = stbi_load(R"(C:\Users\m01016\Git\LearnOpenGL\Texture\textures\awesomeface.png)", &width, &height, &nrChannels, 0);
-
-		if (data)
-		{
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-			glGenerateMipmap(GL_TEXTURE_2D);
-		}
-		else
-		{
-			std::cout << "Failed to load texture\n";
-		}
-		stbi_image_free(data);
-	}
-
-	rectangleShader.set("texture1", 0);
-	rectangleShader.set("texture2", 1);
-
-	// Main loop
-	while (!glfwWindowShouldClose(window)) {
-		processInput(window);
-
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		// Draw axis
-		axisShader.use();
-		glBindVertexArray(vao[0]);
-		glDrawElements(GL_LINES, 4, GL_UNSIGNED_INT, nullptr);
-
-		// Bind textures on corresponding texture units
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture1);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
-
-		// Draw object
-		rectangleShader.use();
-		rectangleShader.set("mixValue", mixValue);
-		glBindVertexArray(vao[1]);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    //-------------------------------------
+    // Bind triangle
+    //-------------------------------------
+    glBindVertexArray(vao[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle_vertices), rectangle_vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[1]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rectangle_indices), rectangle_indices, GL_STATIC_DRAW);
+    // Attributes
+    glVertexAttribPointer(trianglePosAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), static_cast<void*>(nullptr));
+    glEnableVertexAttribArray(trianglePosAttrib);
+    glVertexAttribPointer(triangleColorAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          reinterpret_cast<void*>(3 * sizeof(float)));
+    glEnableVertexAttribArray(triangleColorAttrib);
+    glVertexAttribPointer(triangleTexCoordAttrib, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          reinterpret_cast<void*>(6 * sizeof(float)));
+    glEnableVertexAttribArray(triangleTexCoordAttrib);
 
 
-		// Swap frame buffer
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	glDeleteVertexArrays(2, vao);
-	glDeleteBuffers(2, vbo);
+    //-------------------------------------
+    // load and create a texture
+    //-------------------------------------
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // Set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // Set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    {
+        int width, height, nrChannels;
+        //stbi_set_flip_vertically_on_load(true);
+        unsigned char* data = stbi_load(R"(C:\Users\m01016\Git\LearnOpenGL\Texture\textures\container.jpg)", &width,
+                                        &height, &nrChannels, 0);
 
-	glfwTerminate();
-	return 0;
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0,GL_RGB, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+        else
+        {
+            std::cout << "Failed to load texture\n";
+        }
+
+        stbi_image_free(data);
+    }
+
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // Set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    {
+        int width, height, nrChannels;
+        //stbi_set_flip_vertically_on_load(true);
+        unsigned char* data = stbi_load(R"(C:\Users\m01016\Git\LearnOpenGL\Texture\textures\awesomeface.png)", &width,
+                                        &height, &nrChannels, 0);
+
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+        else
+        {
+            std::cout << "Failed to load texture\n";
+        }
+        stbi_image_free(data);
+    }
+
+    rectangleShader.set("texture1", 0);
+    rectangleShader.set("texture2", 1);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Draw axis
+        axisShader.use();
+        glBindVertexArray(vao[0]);
+        glDrawElements(GL_LINES, 4, GL_UNSIGNED_INT, nullptr);
+
+        // Bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        // Draw object
+        rectangleShader.use();
+        rectangleShader.set("mixValue", mixValue);
+        glBindVertexArray(vao[1]);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+
+        // Swap frame buffer
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(2, vao);
+    glDeleteBuffers(2, vbo);
+
+    glfwTerminate();
+    return 0;
 }
